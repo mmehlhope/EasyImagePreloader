cbd34291f06b606bd2afb82d83882e41a771d684
Matt Mehlhope <mmehlhope@gmail.com>
2012-10-10 18:52:35 -0400

    Converting tabs back to spaces for dev version

--- js/jquery.EasyImagePreloader-1.0-dev.js
+++ js/jquery.EasyImagePreloader-1.0-dev.js
@@ -47,105 +47,105 @@
 
 ;(function($, undefined) {
 
- 	var easyImagePreloader = {
+  var easyImagePreloader = {
 
-		defaults: {
-			useFadeDelay: false, // set to true to enable staggered fade in effect
-			fadeDelay: 100, // delay between staggered fade
-			preload_parent: "i",
-			checkIntervalDelay: 300, // how often the images are checked for their loading state, in milliseconds
-			fadein: 500, // the time in milliseconds it takes for an image to fade in
-			onDone: function() {}, // callback for when all images are complete
-			onEachLoad: function( image ){} // @param image -- callback after each image is loaded
-		},
+    defaults: {
+      useFadeDelay: false, // set to true to enable staggered fade in effect
+      fadeDelay: 100, // delay between staggered fade
+      preload_parent: "i",
+      checkIntervalDelay: 300, // how often the images are checked for their loading state, in milliseconds
+      fadein: 500, // the time in milliseconds it takes for an image to fade in
+      onDone: function() {}, // callback for when all images are complete
+      onEachLoad: function( image ){} // @param image -- callback after each image is loaded
+    },
 
-		init: function( options ) {
+    init: function( options ) {
 
-			return this.each(function() {
+      return this.each(function() {
 
-				var self = easyImagePreloader,
-					$this = $(this), 
-					settings = $.extend(self.defaults, options),
-					images = $this.find("img").css({ "visibility" : "hidden" , opacity: 0 }), 
-					counter = 0,
-					imageCompletionArray = [],
-					checkImagesInterval,
-					fadeDelay = settings.fadeDelay;
+        var self = easyImagePreloader,
+          $this = $(this), 
+          settings = $.extend(self.defaults, options),
+          images = $this.find("img").css({ "visibility" : "hidden" , opacity: 0 }), 
+          counter = 0,
+          imageCompletionArray = [],
+          checkImagesInterval,
+          fadeDelay = settings.fadeDelay;
 
-				//
-				// Loops through the images in the selector and wraps them in the preloader
-				//
-				images.each(function(index) {
+        //
+        // Loops through the images in the selector and wraps them in the preloader
+        //
+        images.each(function(index) {
 
-					var $this = $(this),
-						preloaderCSS = {
-							background: "#fff url(http://cdn.mehlhope.net/images/easyImagePreloader/loadingAnimation.gif) center center no-repeat",
-							display: "block",
-						};
+          var $this = $(this),
+            preloaderCSS = {
+              background: "#fff url(http://cdn.mehlhope.net/images/easyImagePreloader/loadingAnimation.gif) center center no-repeat",
+              display: "block",
+            };
 
-					if ( $this.parent(settings.preload_parent).length == 0 ) {
-						$this.wrap( $('<i/>').css(preloaderCSS) );
-					}
-					else {
-						$this.parent('i').addClass("preloader");
-					}
+          if ( $this.parent(settings.preload_parent).length == 0 ) {
+            $this.wrap( $('<i/>').css(preloaderCSS) );
+          }
+          else {
+            $this.parent('i').addClass("preloader");
+          }
 
-					// Flag each index of the array as false for each item that is detected on the page
-					imageCompletionArray[index] = false;
+          // Flag each index of the array as false for each item that is detected on the page
+          imageCompletionArray[index] = false;
 
-				});		
+        });    
 
-				//
-				// Loop that is run to check the state of the images
-				//
-				function checkImages() {
+        //
+        // Loop that is run to check the state of the images
+        //
+        function checkImages() {
 
-					if ( counter >= imageCompletionArray.length ) {
-						clearInterval(checkImagesInterval);
-						// run onDone callback
-						settings.onDone();
-						return;
-					}
-					for ( var i = 0 ; i < images.length; i++ ) {
+          if ( counter >= imageCompletionArray.length ) {
+            clearInterval(checkImagesInterval);
+            // run onDone callback
+            settings.onDone();
+            return;
+          }
+          for ( var i = 0 ; i < images.length; i++ ) {
 
-						if( images[i].complete === true ) {
-							
-							if (imageCompletionArray[i] === false) {
+            if( images[i].complete === true ) {
+              
+              if (imageCompletionArray[i] === false) {
 
-								// Flag this image as complete in the array
-								imageCompletionArray[i] = true;
-								// run the per-image callback
-								settings.onEachLoad(images[i]);
-								// increment the fade delay
-								fadeDelay = fadeDelay + settings.fadeDelay;
+                // Flag this image as complete in the array
+                imageCompletionArray[i] = true;
+                // run the per-image callback
+                settings.onEachLoad(images[i]);
+                // increment the fade delay
+                fadeDelay = fadeDelay + settings.fadeDelay;
 
-								counter++;
-							}
-							
-							// Fade in the image since it is now loaded
-							$(images[i]).css("visibility", "visible").delay(settings.useFadeDelay ? fadeDelay : 0).animate({opacity: 1}, settings.fadein, function() { 
-								$(this).parent('i').removeAttr('style');
-							});
-						}
-					}
-				}
+                counter++;
+              }
+              
+              // Fade in the image since it is now loaded
+              $(images[i]).css("visibility", "visible").delay(settings.useFadeDelay ? fadeDelay : 0).animate({opacity: 1}, settings.fadein, function() { 
+                $(this).parent('i').removeAttr('style');
+              });
+            }
+          }
+        }
 
-				// Start the interval to check image states
-				checkImagesInterval = setInterval(checkImages, settings.checkIntervalDelay);
-			});
-		}
-	};
+        // Start the interval to check image states
+        checkImagesInterval = setInterval(checkImages, settings.checkIntervalDelay);
+      });
+    }
+  };
 
-	// lets define the actual plugin!
-	$.fn.easyImagePreloader = function( method ) {
+  // lets define the actual plugin!
+  $.fn.easyImagePreloader = function( method ) {
 
-	    if ( easyImagePreloader[method] ) {
-			return easyImagePreloader[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || ! method ) {
-			return easyImagePreloader.init.apply( this, arguments );
-		} else {
-			$.error( 'Method ' +  method + ' does not exist for $.easyImagePreloader' );
-		} 	
-	};
+      if ( easyImagePreloader[method] ) {
+      return easyImagePreloader[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return easyImagePreloader.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist for $.easyImagePreloader' );
+    }   
+  };
 
 })(jQuery);
